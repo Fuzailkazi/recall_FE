7aeb14efc78ba694a9cdea29e89bf6e9b728b4e4
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2024-12-01 17:10:23 +0530

    created iconprops

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Button } from './components/ui/Button';
+import { PlusIcon } from './icons/PlusIcon';
 
 function App() {
   return (
     <>
-      <Button variant='primary' text='Click' size='sm' />
+      <Button
+        variant='primary'
+        text='Click'
+        size='md'
+        startIcon={<PlusIcon />}
+      />
       <Button variant='secondary' text='Click' size='lg' />
     </>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -18,10 +18,10 @@ const variantStyles = {
 const sizeStyles = {
   sm: 'py-1 px-2',
   md: 'py-2 px-4',
-  lg: 'py-3 px-6',
+  lg: 'py-4 px-6',
 };
 
-const defaultStyles = 'rounded-md p-4';
+const defaultStyles = 'rounded-md flex';
 
 export const Button = (props: ButtonProps) => {
   return (
@@ -30,7 +30,9 @@ export const Button = (props: ButtonProps) => {
         sizeStyles[props.size]
       }`}
     >
+      {props.startIcon ? <div className='pr-2'> {props.startIcon} </div> : null}
       {props.text}
+      {props.endIcon}
     </button>
   );
 };
